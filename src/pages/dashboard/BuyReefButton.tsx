dfd47cd0efd5ee3bd5c045482b030519dbc798bf
matjazonline <matjazonline@gmail.com>
2023-04-26 23:51:32 +0200

    buy button external link

--- src/pages/dashboard/BuyReefButton.tsx
+++ src/pages/dashboard/BuyReefButton.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import Uik from '@reef-defi/ui-kit';
 import './buy-reef-button.css';
-import { BUY_URL } from '../../urls';
 
 const Shape = (): JSX.Element => (
   <svg
@@ -235,13 +234,25 @@ C226.3,333.5,225.4,331.3,224.1,329.3z"
 
 const BuyReefButton = (): JSX.Element => {
   const history = useHistory();
-
+  console.log('h', history);
+  /*
   const navigate = (): void => {
     history.push(BUY_URL);
-  };
+  }; */
 
   return (
-    <button
+    <a
+      className="buy-reef-btn"
+      href="https://www.binance.com/en/trade/REEF_USDT?theme=dark&type=spot"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <Uik.ReefSign className="buy-reef-btn__icon" />
+      <span className="buy-reef-btn__text">Buy Reef</span>
+      <Uik.Bubbles />
+      <Shape />
+    </a>
+  /* <button
       type="button"
       className="buy-reef-btn"
       onClick={navigate}
@@ -250,7 +261,7 @@ const BuyReefButton = (): JSX.Element => {
       <span className="buy-reef-btn__text">Buy Reef</span>
       <Uik.Bubbles />
       <Shape />
-    </button>
+    </button> */
   );
 };
 
