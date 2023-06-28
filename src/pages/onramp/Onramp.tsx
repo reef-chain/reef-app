d9cbe52d7e840eb95e0ae9998aa25f4c5c9d0ab6
Anukul Pandey <anukul.030601@gmail.com>
2023-06-28 11:57:26 +0530

    feat: set selected account address

--- src/pages/onramp/Onramp.tsx
+++ src/pages/onramp/Onramp.tsx
@@ -1,12 +1,15 @@
+import { ReefSigner, appState,hooks } from '@reef-defi/react-lib';
 import React from 'react';
 
 function Onramp() {
+const signer: ReefSigner|undefined|null = hooks.useObservableState(appState.selectedSigner$);
   return (
     <div style={{height:'100vh'}}>
       <iframe
-        src="https://onramp.money/main/buy/?appId=487411&walletAddress=5FbG3RL7ftBhHm9eaZ3EDRVWJEFpF8ohct3JeohZdmiF8oDb"
+        src={`https://onramp.money/main/buy/?appId=487411&walletAddress=${signer?.address}`}
         style={{ width: '100%', height: '100%', border: 'none' }}
       ></iframe>
+     
     </div>
   );
 }
