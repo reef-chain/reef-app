d9e0c339ce72ab6bf7b417d56d6804eab8f7b123
Anukul Pandey <anukul.030601@gmail.com>
2023-07-04 15:19:14 +0530

    feat: added overlayNFt

--- src/pages/dashboard/NFT.tsx
+++ src/pages/dashboard/NFT.tsx
@@ -1,41 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BigNumber } from 'ethers';
+import OverlaySend from '../../common/OverlaySend';
+import OverlayNFT from '../../common/OverlayNFT';
 
 interface NFTData {
-    iconUrl: string;
-    name: string;
-    balance: BigNumber;
-    mimetype?: string;
+  iconUrl: string;
+  name: string;
+  balance: BigNumber;
+  mimetype?: string;
+  setKeyIndex: any;
+  keyIndex: any;
 }
 
 const NFT = ({
-  iconUrl, name, balance, mimetype,
+  iconUrl, name, balance, mimetype, setKeyIndex, keyIndex
 }: NFTData): JSX.Element => {
   const loading = false;
+  const [isNftDetailsOpen, setIsNFTDetailsOpen] = useState(false);
+  const [nftKey, setNftKey] = useState(0);
+
+  useEffect(() => {
+    // Reset the key when isNftDetailsOpen is set to false
+    if (!isNftDetailsOpen) {
+      setNftKey(nftKey + 1);
+    }
+  }, [isNftDetailsOpen]);
+
   return (
-    <div className="nfts__item" onClick={()=>console.log('clicked')}>
-      { mimetype && mimetype.indexOf('mp4') > -1
-          && (
+    <div className="nfts__item" onClick={() => {
+      setIsNFTDetailsOpen(true);
+    }}>
+      {mimetype && mimetype.indexOf('mp4') > -1
+        && (
           <video className="nfts__item-video" autoPlay loop muted poster="">
             <source src={iconUrl} type="video/mp4" />
           </video>
-          )}
-      { (!mimetype || !(mimetype?.indexOf('mp4') > -1)) && (
-      <div
-        className={`
-              nfts__item-image
-              ${loading ? 'nfts__item-image--loading' : ''}
+        )}
+      {(!mimetype || !(mimetype?.indexOf('mp4') > -1)) && (
+        <div
+          className={`
+            nfts__item-image
+            ${loading ? 'nfts__item-image--loading' : ''}
           `}
-        style={
-                iconUrl && !loading
-                  ? { backgroundImage: `url(${iconUrl})` } : {}
-            }
-      />
+          style={
+            iconUrl && !loading
+              ? { backgroundImage: `url(${iconUrl})` } : {}
+          }
+        />
       )}
       <div className="nfts__item-info">
         <div className="nfts__item-name">{name}</div>
         <div className="nfts__item-balance">{balance.toString()}</div>
       </div>
+      <OverlayNFT
+        key={nftKey}
+        nftName={name}
+        isVideoNFT={mimetype != undefined && mimetype.indexOf('mp4') > -1}
+        iconUrl={iconUrl}
+        isOpen={isNftDetailsOpen}
+        onClose={setKeyIndex}
+        onCloseIdx={keyIndex}
+      />
     </div>
   );
 };
--- src/pages/dashboard/Nfts.tsx
+++ src/pages/dashboard/Nfts.tsx
@@ -1,13 +1,10 @@
 import { NFT as NFTData } from '@reef-defi/react-lib';
-import React from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import './Nfts.css';
 import Uik from '@reef-chain/ui-kit';
 import NFT from './NFT';
 import SqwidButton from './SqwidButton/SqwidButton';
 import { localizedStrings } from '../../l10n/l10n';
-
-// const { isDataSet, DataProgress } = utils;
-// const placeholderImage = 'https://cryptotelegram.com/wp-content/uploads/2021/04/reef-crypto-explained.jpg';
 
 export const Skeleton = (): JSX.Element => (
   <div className="nft-skeleton">
@@ -16,36 +13,45 @@ export const Skeleton = (): JSX.Element => (
   </div>
 );
 
-interface Nfts {
+interface NftsProps {
   nfts: NFTData[];
 }
 
-export const Nfts = ({ nfts }: Nfts): JSX.Element => (
-  <div className="nfts">
+export const Nfts = ({ nfts }: NftsProps): JSX.Element => {
+  const [indexKey, setIndexKey] = useState(0);
+  const keyIndex = useMemo(() => indexKey, [indexKey]);
 
-    {nfts.length === 0 && (
-    <div className="nfts__empty">
-      <Uik.Text type="light">{localizedStrings.does_not_hold}</Uik.Text>
-      <SqwidButton />
-    </div>
-    )}
-    {nfts.length > 0 && (
-      <div className="col-12">
-        {!!nfts.length && (
-        <div className="nfts__container">
-          {nfts.map((nft) => (
-            <NFT
-              key={`${nft.address}-${nft.nftId}`}
-              iconUrl={nft.iconUrl}
-              name={nft.name}
-              balance={nft.balance}
-              mimetype={nft.mimetype}
-            />
-          ))}
+  useEffect(() => {
+    // Do something when indexKey changes
+  }, [indexKey]);
+
+  return (
+    <div className="nfts">
+      {nfts.length === 0 && (
+        <div className="nfts__empty">
+          <Uik.Text type="light">{localizedStrings.does_not_hold}</Uik.Text>
+          <SqwidButton />
         </div>
-        )}
-      </div>
-    )}
-  </div>
-
-);
+      )}
+      {nfts.length > 0 && (
+        <div className="col-12">
+          {!!nfts.length && (
+            <div className="nfts__container">
+              {nfts.map((nft, index) => (
+                <NFT
+                  key={`${nft.address}-${nft.nftId}-${indexKey}`}
+                  iconUrl={nft.iconUrl}
+                  name={nft.name}
+                  balance={nft.balance}
+                  mimetype={nft.mimetype}
+                  setKeyIndex={setIndexKey}
+                  keyIndex={keyIndex}
+                />
+              ))}
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
