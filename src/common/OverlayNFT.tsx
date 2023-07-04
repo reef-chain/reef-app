24034e7108ed8276449580112cb0abc7c769f609
Anukul Pandey <anukul.030601@gmail.com>
2023-07-04 15:19:14 +0530

    feat: added overlayNFt

---
+++ src/common/OverlayNFT.tsx
@@ -0,0 +1,57 @@
+import { appState, Components, hooks } from '@reef-defi/react-lib';
+import React, { useContext,useState } from 'react';
+import TokenContext from '../context/TokenContext';
+import { notify } from '../utils/utils';
+import './overlay-swap.css';
+
+const { Send, OverlayAction } = Components;
+
+export interface OverlayNFT {
+  nftName?: string;
+  isOpen: boolean;
+  isVideoNFT: boolean;
+  iconUrl: string;
+  onClose:any;
+  onCloseIdx:any;
+}
+
+const OverlayNFT = ({
+  nftName,
+  isOpen,
+  isVideoNFT,
+  iconUrl,
+  onClose,
+  onCloseIdx
+}: OverlayNFT): JSX.Element => {
+  const { tokens } = useContext(TokenContext);
+
+  const signer = hooks.useObservableState(appState.selectedSigner$);
+  const accounts = hooks.useObservableState(appState.accountsSubj);
+  const provider = hooks.useObservableState(appState.currentProvider$);
+
+  return (
+    <OverlayAction
+      isOpen={isOpen}
+      title="NFT Details"
+      onClose={()=>
+        onClose(onCloseIdx+1)
+    }
+      className="overlay-swap"
+    >
+      <div className="uik-pool-actions pool-actions">
+        <div>{nftName}</div>
+        <div>
+          {isVideoNFT ? (
+            <video className="nfts__item-video" autoPlay loop muted poster="">
+              <source src={iconUrl} type="video/mp4" />
+            </video>
+          ) : (
+            <img src={iconUrl} alt="" />
+          )}
+        </div>
+      </div>
+    </OverlayAction>
+  );
+};
+
+export default OverlayNFT;
