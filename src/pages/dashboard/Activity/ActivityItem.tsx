64085ed73ebf92ac42758596b648ddfad752c02f
Metod Ribič <metodribic@users.noreply.github.com>
2023-08-01 10:31:46 +0200

    show video icon in activity if item is mp4 NFT (#28)
    * show video icon in activity if item is mp4 NFT
    
    * resolve ipfs url
    
    * lint
    
    ---------
    
    Co-authored-by: Metod Ribič <metod.ribic@better.care>
    Co-authored-by: matjazonline <matjazonline@gmail.com>

--- src/pages/dashboard/Activity/ActivityItem.tsx
+++ src/pages/dashboard/Activity/ActivityItem.tsx
@@ -34,9 +34,18 @@ const TokenActivityItem = ({
   url,
 }: Props): JSX.Element => {
   // @ts-ignore-next-line
-  const isNFT = !!(token as NFT).nftId;
+  const {
+    symbol,
+    name,
+    iconUrl,
+  } = token;
+  const {
+    nftId,
+    mimetype,
+  } = token as NFT;
+  const isNFT = nftId != null;
   const type: 'receive' | 'send' = inbound ? 'receive' : 'send';
-
+  console.log('iiiii=', iconUrl);
   const title = useMemo(() => {
     const actionMap = {
       receive: strings.received,
@@ -44,8 +53,8 @@ const TokenActivityItem = ({
     };
 
     const action = actionMap[type];
-    return `${action} ${token.symbol || token.name}`;
-  }, [type, token.symbol]);
+    return `${action} ${symbol || name}`;
+  }, [type, symbol, name]);
 
   const amount = useMemo(() => {
     const amt = displayBalanceFromToken(token as Token);
@@ -59,6 +68,46 @@ const TokenActivityItem = ({
   }, [token, type]);
 
   const hideBalance = useContext(HideBalance);
+
+  const activityPreviewIcon = useMemo(() => {
+    const iconUrlIpfsResolved = iconUrl.startsWith('ipfs') ? getIpfsGatewayUrl(iconUrl.substring(7)) : iconUrl;
+
+    const isVideoNFT = mimetype && mimetype.indexOf('mp4') > -1;
+
+    if (!isNFT) {
+      return (
+        <div
+          className="activity-item__amount-token-icon"
+          style={{ backgroundImage: `url(${iconUrlIpfsResolved})` }}
+        />
+      );
+    }
+
+    return isVideoNFT ? (
+      <div
+        className="activity-item__nft-video-icon activity-item__nft-preview"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 122.88 111.34">
+          <path
+            fill="currentColor"
+            d="M23.59,0h75.7a23.68,23.68,0,0,1,23.59,23.59V87.75A23.56,23.56,0,0,1,116,104.41l-.22.2a23.53,23.53,0,0,
+            1-16.44,6.73H23.59a23.53,23.53,0,0,1-16.66-6.93l-.2-.22A23.46,23.46,0,0,1,0,87.75V23.59A23.66,23.66,0,0,1,
+            23.59,0ZM54,47.73,79.25,65.36a3.79,3.79,0,0,1,.14,6.3L54.22,89.05a3.75,3.75,0,0,1-2.4.87A3.79,3.79,0,0,1,
+            48,86.13V50.82h0A3.77,3.77,0,0,1,54,47.73ZM7.35,26.47h14L30.41,7.35H23.59A16.29,16.29,0,0,0,7.35,
+            23.59v2.88ZM37.05,7.35,28,26.47H53.36L62.43,7.38v0Zm32,0L59.92,26.47h24.7L93.7,7.35Zm31.32,0L91.26,
+            26.47h24.27V23.59a16.32,16.32,0,0,0-15.2-16.21Zm15.2,26.68H7.35V87.75A16.21,16.21,0,0,0,12,
+            99.05l.17.16A16.19,16.19,0,0,0,23.59,104h75.7a16.21,16.21,0,0,0,11.3-4.6l.16-.18a16.17,16.17,0,0,0,
+            4.78-11.46V34.06Z"
+          />
+        </svg>
+      </div>
+    ) : (
+      <div
+        className="activity-item__nft-preview"
+        style={{ backgroundImage: `url(${iconUrlIpfsResolved})` }}
+      />
+    );
+  }, [mimetype, isNFT, iconUrl]);
 
   return (
     <a
@@ -84,10 +133,7 @@ const TokenActivityItem = ({
 
         {
           isNFT ? (
-            <div
-              className="activity-item__nft-preview"
-              style={{ backgroundImage: `url(${token.iconUrl})` }}
-            />
+            activityPreviewIcon
           ) : (
             <div
               className="activity-item__amount-wrapper"
@@ -111,10 +157,7 @@ const TokenActivityItem = ({
                 )
               }
               </div>
-              <div
-                className="activity-item__amount-token-icon"
-                style={{ backgroundImage: `url(${token.iconUrl.startsWith('ipfs') ? getIpfsGatewayUrl(token.iconUrl.substring(7)) : token.iconUrl})` }}
-              />
+              { activityPreviewIcon }
             </div>
           )
         }
