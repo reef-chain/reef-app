6afb7549514ddeed1b5e86777c05b6e863e4fef3
Anukul Pandey <anukul.030601@gmail.com>
2023-09-07 14:33:47 +0530

    feat: initial setup

---
+++ src/useInitReefState.ts
@@ -0,0 +1,48 @@
+import {
+    reefState,
+  } from '@reef-chain/util-lib';
+  import { useEffect, useState } from 'react';
+  import { useInjectExtension } from './useInjectExtension';
+  import { Provider } from '@reef-defi/evm-provider';
+import { Network} from '@reef-defi/react-lib';
+import { useObservableState } from './hooks/useObservableState';
+import { StateOptions } from '@reef-defi/react-lib/dist/appState/util';
+import { State } from '@reef-defi/react-lib/dist/appState/util';
+  
+  export const useInitReefState = (
+    applicationDisplayName: string,
+    options: StateOptions = {},
+  ): State => {
+    const {
+      network, ipfsHashResolverFn,
+    } = options;
+    const [accounts, extension, loadingExtension, errExtension] = useInjectExtension(applicationDisplayName);
+    const selectedNetwork: Network|undefined = useObservableState(reefState.selectedNetwork$);
+    const provider = useObservableState(reefState.selectedProvider$) as Provider|undefined;
+    const [loading, setLoading] = useState(true);
+  
+    useEffect(() => {
+  
+      if (!accounts || !accounts.length || !extension) {
+        return;
+      }
+  
+      const jsonAccounts = { accounts, injectedSigner: extension?.signer };
+        reefState.initReefState({
+          network,
+          jsonAccounts,
+          ipfsHashResolverFn,
+        });
+    }, [accounts, extension]);
+  
+    useEffect(() => {
+      setLoading(loadingExtension||provider==undefined);
+    }, [loadingExtension,provider]);
+  
+    return {
+      error:errExtension,
+      loading,
+      provider,
+      network: selectedNetwork,
+    };
+  };
