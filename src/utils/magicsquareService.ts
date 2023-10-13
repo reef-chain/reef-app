37d84d49d64df2ae81eb3c177f0403e4cf403002
Anukul Pandey <anukul.030601@gmail.com>
2023-10-13 19:27:26 +0530

    chore: lint

--- src/utils/magicsquareService.ts
+++ src/utils/magicsquareService.ts
@@ -1,10 +1,10 @@
-import axios from "axios";
-import { useLocation } from "react-router";
+import axios from 'axios';
+import { useLocation } from 'react-router';
 
-const MS_PROMO_IDENT = "msPromo";
+const MS_PROMO_IDENT = 'msPromo';
 
-export const saveToMs = async ()=>{
-    // save to magicstore - remove after campaign @anukulpandey
+export const useSaveToMs = async ():Promise<void> => {
+  // save to magicstore - remove after campaign @anukulpandey
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const params = {};
@@ -13,15 +13,17 @@ export const saveToMs = async ()=>{
     // @ts-ignore
     params[key] = value;
   }
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const existingParams = JSON.parse(localStorage.getItem(MS_PROMO_IDENT) as any) || {};
-    const updatedParams = { ...existingParams, ...params };
-    localStorage.setItem('msPromo', JSON.stringify(updatedParams));
-}
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const existingParams = JSON.parse(localStorage.getItem(MS_PROMO_IDENT) as any) || {};
+  const updatedParams = { ...existingParams, ...params };
+  localStorage.setItem('msPromo', JSON.stringify(updatedParams));
+};
 
-export const makeRequestToMs = async (baseUrl:string)=>{
-    const bodyParams = JSON.parse(localStorage.getItem(MS_PROMO_IDENT) as any) || {};
-    // eslint-disable-next-line no-param-reassign
-    const {data}=await axios.post(`${baseUrl}/magicsquare`,bodyParams)
-    return data;
-}
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const makeRequestToMs = async (baseUrl:string):Promise<any> => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const bodyParams = JSON.parse(localStorage.getItem(MS_PROMO_IDENT) as any) || {};
+  // eslint-disable-next-line no-param-reassign
+  const { data } = await axios.post(`${baseUrl}/magicsquare`, bodyParams);
+  return data;
+};
