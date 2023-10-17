6fcfd21bc6d431f67c9d8b81d7986d5e94b19d17
matjazonline <matjazonline@gmail.com>
2023-10-17 13:42:48 +0200

    cleanup magicsquareService

--- src/utils/magicsquareService.ts
+++ src/utils/magicsquareService.ts
@@ -1,14 +1,25 @@
 import axios from 'axios';
 import { useLocation } from 'react-router';
+import { AvailableNetworks } from '@reef-defi/react-lib/dist/state/network';
 
 const MS_PROMO_IDENT = 'msPromo';
+
+type NetworksMap<T> = { [NET in AvailableNetworks]: T };
+const baseUrls: NetworksMap<string> = {
+  mainnet: 'https://api.reefscan.com',
+  testnet: 'https://api-testnet.reefscan.com',
+  localhost: 'https://api-testnet.reefscan.com',
+};
+
+export enum EventType{
+  SWAP = 'swap',
+}
 
 interface paramType {
   vid?:string
 }
 
 export const useSaveToMs = async ():Promise<void> => {
-  // save to magicstore - remove after campaign @anukulpandey
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const params:paramType = {};
@@ -18,32 +29,28 @@ export const useSaveToMs = async ():Promise<void> => {
     params[key] = value;
   }
 
-  if (!params.vid) return;
+  if (!params.vid) {
+    return;
+  }
 
-  const updatedParams = {
-    msUserId: params.vid,
-  };
-
-  localStorage.setItem(MS_PROMO_IDENT, JSON.stringify(updatedParams));
+  localStorage.setItem(MS_PROMO_IDENT, params.vid);
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const makeRequestToMs = async (eventType:string, address:string):Promise<any> => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const msData = JSON.parse(localStorage.getItem(MS_PROMO_IDENT) as any) || {};
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const selectedNetwork = JSON.parse(localStorage.getItem('reef-app-active-network') as any);
+export const magicSquareAction = async (network: AvailableNetworks, eventType: EventType, address:string):Promise<void> => {
+  const baseUrl = baseUrls[network];
+  const msUserId = localStorage.getItem(MS_PROMO_IDENT);
 
-  if (!msData.vid) return null;
+  if (!msUserId) {
+    return;
+  }
 
   const bodyParams = {
-    msUserId: msData.vid,
-    network: selectedNetwork.name,
+    msUserId,
+    network,
     eventType,
     address,
   };
 
-  // eslint-disable-next-line no-param-reassign
-  const { data } = await axios.post(`${selectedNetwork.verificationApiUrl}/magicsquare`, bodyParams);
-  return data;
+  axios.post(`${baseUrl}/magicsquare`, bodyParams);
 };
