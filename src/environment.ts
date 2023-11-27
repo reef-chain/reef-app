9eae62b642b760e5e965e4ddb44b154d922d20ea
matjazonline <matjazonline@gmail.com>
2023-11-27 14:00:06 +0100

    use rpc from reefscan.com

--- src/environment.ts
+++ src/environment.ts
@@ -5,8 +5,8 @@ export const isReefswapUI = window.location.host.indexOf('reefswap') > -1;
 
 if (isReefswapUI) {
   console.warn('Setting testnet for reefswap promo period!');
-  localStorage.setItem('reef-app-active-network', '{"name":"testnet","rpcUrl":"wss://rpc-testnet.reefscan.info/ws","reefscanUrl":"https://testnet.reefscan.info","verificationApiUrl":"https://api-testnet.reefscan.com","factoryAddress":"0x06D7a7334B9329D0750FFd0a636D6C3dFA77E580","routerAddress":"0xa29DFc7329ac30445Ba963E313fD26E171722057","graphqlExplorerUrl":"wss://squid.subsquid.io/reef-explorer-testnet/graphql","graphqlDexsUrl":"https://squid.subsquid.io/reef-swap-testnet/graphql","genesisHash":"0x0f89efd7bf650f2d521afef7456ed98dff138f54b5b7915cc9bce437ab728660","bonds":[]}');
-  document.title = 'ReefSwap dApp';
+  localStorage.setItem('reef-app-active-network', '{"name":"testnet","rpcUrl":"wss://rpc-testnet.reefscan.com/ws","reefscanUrl":"https://testnet.reefscan.com","verificationApiUrl":"https://api-testnet.reefscan.com","factoryAddress":"0x06D7a7334B9329D0750FFd0a636D6C3dFA77E580","routerAddress":"0xa29DFc7329ac30445Ba963E313fD26E171722057","graphqlExplorerUrl":"wss://squid.subsquid.io/reef-explorer-testnet/graphql","graphqlDexsUrl":"https://squid.subsquid.io/reef-swap-testnet/graphql","genesisHash":"0x0f89efd7bf650f2d521afef7456ed98dff138f54b5b7915cc9bce437ab728660","bonds":[]}');
+  document.title = 'ReefSwap DEX';
 }
 export const getIpfsGatewayUrl = (hash: string): string => `https://reef.infura-ipfs.io/ipfs/${hash}`;
 export const testnetOverride = {
