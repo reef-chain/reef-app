051564fdcce7612591498fe5438875f60bba37e4
Anukul Pandey <anukul.030601@gmail.com>
2024-01-18 18:18:42 +0530

    chore: lint

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './assets/index.css';
 import '@reef-chain/react-lib/dist/index.css';
 import {
   BrowserRouter as Router,
 } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 import { version } from '../package.json';
-import { createRoot } from 'react-dom/client';
 
 console.log(`Reef-app version: ${version}`);
 
