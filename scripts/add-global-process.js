b81d9ebdd7340d9a87e40e9ed02b2a903b311683
Anukul Pandey <anukul.030601@gmail.com>
2024-10-07 13:30:33 +0530

    feat: disable warnings with var

--- scripts/add-global-process.js
+++ scripts/add-global-process.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
-const filePath = path.resolve(__dirname, '../node_modules/@reef-chain/ui-kit/dist/index.es.js');
+const filesToModify = [
+  path.resolve(__dirname, '../node_modules/@polkadot/util/detectPackage.js'),
+  path.resolve(__dirname, '../node_modules/@reef-chain/ui-kit/dist/index.es.js'),
+];
 
 const contentToAdd = `
 globalThis.process = {};
@@ -10,19 +13,24 @@ globalThis.process.env = {
 };
 `;
 
-fs.readFile(filePath, 'utf8', (err, data) => {
-  if (err) {
-    console.error('Error reading file:', err);
-    return;
-  }
-
-  const updatedContent = contentToAdd + data;
-
-  fs.writeFile(filePath, updatedContent, 'utf8', (err) => {
+const modifyFile = (filePath) => {
+  fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
-      console.error('Error writing to file:', err);
-    } else {
-      console.log('File successfully updated.');
+      console.error(`Error reading file: ${filePath}`, err);
+      return;
     }
+
+    const updatedContent = contentToAdd + data;
+
+    fs.writeFile(filePath, updatedContent, 'utf8', (err) => {
+      if (err) {
+        console.error(`❌ Error writing to file: ${filePath}`, err);
+      } else {
+        console.log(`✅ Globals added successfully: ${filePath}`);
+      }
+    });
   });
-});
+};
+
+filesToModify.forEach(modifyFile);
+console.log(`⚠️ Disabling @polkadot/X warnings!`);
