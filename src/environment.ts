eea0f0dc5c8f182e2c1ef01a002b3f94d4bd2741
Anukul Pandey <anukul.030601@gmail.com>
2025-12-19 13:31:01 +0530

    fix: evm address emission

--- src/environment.ts
+++ src/environment.ts
@@ -13,6 +13,8 @@ export const binanceConnectApiUrl = 'https://onramp.reefscan.info';
 // generate new one for app.reef.io and replace it here
 export const formoApiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmlnaW4iOiJodHRwczovL3JlZWYuaW8iLCJwcm9qZWN0X2lkIjoid2lsMGJMR1NrUURucWx4TDc4bHgyIiwiaWF0IjoxNzY1OTgyNDI5fQ.aPap3ykS2g1QFg1roeTWrSv7XO47tnh6fl7-xo1NyVQ"; //https://zana-arthrosporic-janie.ngrok-free.dev got this using $ngrok http 3000 --host-header="localhost:3000"
 
+export const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
+
 export const useDexConfig = (network: nw.Network): nw.DexProtocolv2 | undefined => {
   const [dexConfig, setDexConfig] = useState<nw.DexProtocolv2 | undefined>(undefined);
 
