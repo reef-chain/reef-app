7a64f97636ec719e8ed37bdbcca7f03aa204e400
Anukul Pandey <anukul.030601@gmail.com>
2025-12-19 13:32:12 +0530

    feat: update keys

--- src/environment.ts
+++ src/environment.ts
@@ -10,8 +10,7 @@ export const getIpfsGatewayUrl = (hash: string): string => `https://reef.infura-
 export const appAvailableNetworks = [nw.AVAILABLE_NETWORKS.mainnet, nw.AVAILABLE_NETWORKS.testnet];
 export const binanceConnectApiUrl = 'https://onramp.reefscan.info';
 
-// generate new one for app.reef.io and replace it here
-export const formoApiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmlnaW4iOiJodHRwczovL3JlZWYuaW8iLCJwcm9qZWN0X2lkIjoid2lsMGJMR1NrUURucWx4TDc4bHgyIiwiaWF0IjoxNzY1OTgyNDI5fQ.aPap3ykS2g1QFg1roeTWrSv7XO47tnh6fl7-xo1NyVQ"; //https://zana-arthrosporic-janie.ngrok-free.dev got this using $ngrok http 3000 --host-header="localhost:3000"
+export const formoApiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmlnaW4iOiJodHRwczovL2FwcC5yZWVmLmlvIiwicHJvamVjdF9pZCI6IjNSQWZuNGZrR0o4dnI3bERmMHFGZCIsImlhdCI6MTc2NjA3MzgwOH0.1coWZO-PV7JHv5_duD9Wmkyd4ivVygrHqtsaUZEF1S8";
 
 export const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 
