7a7243602a3f150c6397b547d26ba27cfb65f2eb
Anukul Pandey <anukul.030601@gmail.com>
2025-12-23 11:58:02 +0530

    feat: refactoring

--- src/environment.ts
+++ src/environment.ts
@@ -10,7 +10,11 @@ export const getIpfsGatewayUrl = (hash: string): string => `https://reef.infura-
 export const appAvailableNetworks = [nw.AVAILABLE_NETWORKS.mainnet, nw.AVAILABLE_NETWORKS.testnet];
 export const binanceConnectApiUrl = 'https://onramp.reefscan.info';
 
-export const formoApiKey = isReefswapUI?"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmlnaW4iOiJodHRwczovL3JlZWZzd2FwLmNvbS8iLCJwcm9qZWN0X2lkIjoiZnBtUWYySlVuQU5MamZDTWdmSkFzIiwiaWF0IjoxNzY2NDExOTY4fQ.ts-JpGetb6K6Ggn444oZFjH0pLfLqLLbnlaqspeSVhw":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmlnaW4iOiJodHRwczovL2FwcC5yZWVmLmlvIiwicHJvamVjdF9pZCI6IlFtM3ZxTGd4dUZOblpVMmlHV3g2YSIsImlhdCI6MTc2NjE1MTg3Nn0.FZ6x8vm7s72MAtGj4MYOBm3p1R5oFVGPEGPBZO2mj8Y";
+const REEFSWAP_FORMO_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmlnaW4iOiJodHRwczovL3JlZWZzd2FwLmNvbS8iLCJwcm9qZWN0X2lkIjoiZnBtUWYySlVuQU5MamZDTWdmSkFzIiwiaWF0IjoxNzY2NDExOTY4fQ.ts-JpGetb6K6Ggn444oZFjH0pLfLqLLbnlaqspeSVhw";
+
+const REEFAPP_FORMO_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmlnaW4iOiJodHRwczovL2FwcC5yZWVmLmlvIiwicHJvamVjdF9pZCI6IlFtM3ZxTGd4dUZOblpVMmlHV3g2YSIsImlhdCI6MTc2NjE1MTg3Nn0.FZ6x8vm7s72MAtGj4MYOBm3p1R5oFVGPEGPBZO2mj8Y";
+
+export const formoApiKey = isReefswapUI ? REEFSWAP_FORMO_KEY : REEFAPP_FORMO_KEY;
 
 export const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 
@@ -18,7 +22,7 @@ export const useDexConfig = (network: nw.Network): nw.DexProtocolv2 | undefined 
   const [dexConfig, setDexConfig] = useState<nw.DexProtocolv2 | undefined>(undefined);
 
   useEffect(() => {
-    const fetchDexConfig = async ():Promise<void> => {
+    const fetchDexConfig = async (): Promise<void> => {
       try {
         const config = nw.getReefswapNetworkConfig(network);
         setDexConfig(config);
