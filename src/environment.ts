fa9ec78b0ad2a6031d81590e8950bcd18f48a2dd
Anukul Pandey <anukul.030601@gmail.com>
2025-12-23 11:56:50 +0530

    feat: reefswap key conditional rendering

--- src/environment.ts
+++ src/environment.ts
@@ -10,7 +10,7 @@ export const getIpfsGatewayUrl = (hash: string): string => `https://reef.infura-
 export const appAvailableNetworks = [nw.AVAILABLE_NETWORKS.mainnet, nw.AVAILABLE_NETWORKS.testnet];
 export const binanceConnectApiUrl = 'https://onramp.reefscan.info';
 
-export const formoApiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmlnaW4iOiJodHRwczovL2FwcC5yZWVmLmlvIiwicHJvamVjdF9pZCI6IlFtM3ZxTGd4dUZOblpVMmlHV3g2YSIsImlhdCI6MTc2NjE1MTg3Nn0.FZ6x8vm7s72MAtGj4MYOBm3p1R5oFVGPEGPBZO2mj8Y";
+export const formoApiKey = isReefswapUI?"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmlnaW4iOiJodHRwczovL3JlZWZzd2FwLmNvbS8iLCJwcm9qZWN0X2lkIjoiZnBtUWYySlVuQU5MamZDTWdmSkFzIiwiaWF0IjoxNzY2NDExOTY4fQ.ts-JpGetb6K6Ggn444oZFjH0pLfLqLLbnlaqspeSVhw":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmlnaW4iOiJodHRwczovL2FwcC5yZWVmLmlvIiwicHJvamVjdF9pZCI6IlFtM3ZxTGd4dUZOblpVMmlHV3g2YSIsImlhdCI6MTc2NjE1MTg3Nn0.FZ6x8vm7s72MAtGj4MYOBm3p1R5oFVGPEGPBZO2mj8Y";
 
 export const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 
